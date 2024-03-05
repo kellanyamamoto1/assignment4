923fcb80afdd7b936dc499285eb98170b5d3fb73
kellanyamamoto1 <157436045+kellanyamamoto1@users.noreply.github.com>
2024-03-04 20:51:50 -0800

    Class Addition

--- OpenWeather.py
+++ OpenWeather.py
@@ -15,6 +15,30 @@
 import urllib, json
 from urllib import request,error
 
+class OpenWeather():
+
+    def set_apikey(self, apikey:str) -> None:
+        '''    
+        Sets the apikey required to make requests to a web API.
+        :param apikey: The apikey supplied by the API service
+    
+        '''
+        self.api_key = apikey
+        
+
+    def load_data(self) -> None:
+        '''
+        Calls the web api using the required values and stores the response in class data attributes.
+        '''
+        if self.api_key is None:
+            raise ValueError("API key is not set. Please use set_apikey method to set the API key.")
+        
+        url = f"https://api.openweathermap.org/data/2.5/weather?&appid={self.api_key}"
+        response = urllib.request.urlopen(url)
+        data = json.loads(response.read())
+        self.weather_description = data['weather'][0]['description']
+        
+
 def _download_url(url_to_download: str) -> dict:
     response = None
     r_obj = None
@@ -39,6 +63,11 @@ def main() -> None:
     ccode = "US"
     apikey = "ceb8cbc931c2f41301ba4a1548020fd4"
     url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip},{ccode}&appid={apikey}"
+    
+    weather = OpenWeather()
+    weather.set_apikey("your_api_key_here")
+    weather.load_data()
+    print(weather.weather_description)
 
     weather_obj = _download_url(url)
     if weather_obj is not None:
--- a4.py
+++ a4.py
@@ -10,3 +10,20 @@
 # 28388886
 
 
+from OpenWeather import OpenWeather
+
+zipcode = "92697"
+ccode = "US"
+apikey = "ceb8cbc931c2f41301ba4a1548020fd4"
+
+open_weather = OpenWeather(zipcode, ccode)
+open_weather.set_apikey(apikey)
+open_weather.load_data()
+
+print(f"The temperature for {zipcode} is {open_weather.temperature} degrees")
+print(f"The high for today in {zipcode} will be {open_weather.high_temperature} degrees")
+print(f"The low for today in {zipcode} will be {open_weather.low_temperature} degrees")
+print(f"The coordinates for {zipcode} are {open_weather.longitude} longitude and {open_weather.latitude} latitude")
+print(f"The current weather for {zipcode} is {open_weather.description}")
+print(f"The current humidity for {zipcode} is {open_weather.humidity}")
+print(f"The sun will set in {open_weather.city} at {open_weather.sunset}")
