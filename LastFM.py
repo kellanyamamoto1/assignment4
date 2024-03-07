be3a665afc4e39468301579dea8a02b34321a6ec
kellanyamamoto1 <157436045+kellanyamamoto1@users.noreply.github.com>
2024-03-06 19:01:54 -0800

    Json dump of all album info

--- LastFM.py
+++ LastFM.py
@@ -13,27 +13,31 @@
 # SHARED SECRET: 1c62c7b8c077d5ea2a1a70b4cb77b506
 
 
-import urllib, json, datetime
+import urllib, json
 from urllib import request,error
 class LastFM:
 
     FMkey = None
 
+
     def setFMapi(self, FMapikey: str) -> None:
-        self.FMkey = FMapikey
+        self.apiKey = FMapikey
+
+    def set_artist_album(self, artist: str, album: str) -> None:
+        self.artist = artist
+        self.album = album
 
     def loadFMdata(self):
-        if self.FMkey is None:
-            raise ValueError("API key is not set. Please use set_apikey method to set the API key.")
-        
+        if self.apiKey is None:
+            raise ValueError("API key is not set. Please use setFMapi method to set the API key.")
+        if self.artist is None or self.album is None:
+            raise ValueError("Artist or album is not set. Please use set_artist_album method to set the artist and album.")
+
         try:
-            url = f"http://www.last.fm/api/auth/?api_key={self.FMkey}"
+            url = f"http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key={self.apiKey}&artist={self.artist}&album={self.album}&format=json"
             response = urllib.request.urlopen(url)
             data = json.loads(response.read())
-           
-            
-
-
+            return data
 
         except urllib.error.URLError as e:
             raise Exception(f"Lost local connection to the Internet: {e}")
@@ -48,5 +52,16 @@ class LastFM:
             raise Exception("Invalid data formatting from the remote API")
 
 
+
 def main():
     FMapikey = "7cd2ee13dc3b0100dae94c5c7401df50"
+    artist = 'Cher'
+    album = 'Believe'
+    lastfm = LastFM()
+    lastfm.setFMapi(FMapikey)
+    lastfm.set_artist_album(artist, album)
+    data = lastfm.loadFMdata()
+    print(json.dumps(data, indent=4))
+
+if __name__ == "__main__":
+    main()
